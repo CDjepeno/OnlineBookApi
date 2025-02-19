52bc716898cae8ad885d4df0ae11a58734f3dc6a
CDjepeno <djepenodev@gmail.com>
2025-02-19 13:34:34 +0100

    fix: :bug: ON-955 fix husky in  docker

---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test']],
+      'subject-case': [0], // Désactive la vérification de la casse
+      'header-max-length': [2, 'always', 100], // Limite la longueur max
+      'subject-empty': [2, 'never'], // Interdit les messages vides
+    },
+    parserPreset: {
+      parserOpts: {
+        headerPattern: /^(feat|fix|chore|docs|style|refactor|test): (\:sparkles:|\:hammer:|\:bug:) (ON-\d{3,}) (.+)$/,
+        headerCorrespondence: ['type', 'emoji', 'ticket', 'subject'],
+      },
+    },
+  };
+  
