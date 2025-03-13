285c7ddbbef78a45e3c4e09fa1fc3bc8d0cd04a4
CDjepeno <djepenodev@gmail.com>
2025-03-13 21:12:45 +0100

    fix: :hammer: ON-183 refactore getAllBook.useCase for error

--- apps/nest/src/application/usecases/book/getAllBook/getAllBook.usecase.ts
+++ apps/nest/src/application/usecases/book/getAllBook/getAllBook.usecase.ts
@@ -1,4 +1,8 @@
-import { HttpException } from '@nestjs/common';
+import {
+  InternalServerException,
+  NotFoundException,
+} from 'src/domaine/errors/onlineBook.error';
+import { ErrorsMessagesEnum } from 'src/enums/errors.enums';
 import { BookRepository } from 'src/repositories/book.repository';
 import { GetAllBookResponsePagination } from './getAllBook.response';
 
@@ -11,9 +15,18 @@ export class GetAllBookUsecase {
     try {
       return await this.repository.getAllBook(page, limit);
     } catch (error) {
-      if (error instanceof HttpException) {
-        // Si c'est une exception NestJS connue, on la relance directement
-        throw error;
+      if (error instanceof Error) {
+        if (error.message === ErrorsMessagesEnum.INTERNAL_SERVER_ERROR) {
+          throw new InternalServerException(
+            'Probleme serveur impossible de supprimer le livre',
+          );
+        }
+        if (error.message === ErrorsMessagesEnum.DATABASE_ERROR) {
+          throw new InternalServerException('Database Error');
+        }
+        if (error.message === ErrorsMessagesEnum.NOT_FOUND) {
+          throw new NotFoundException('Aucun livres trouvé');
+        }
       }
       throw error;
     }
