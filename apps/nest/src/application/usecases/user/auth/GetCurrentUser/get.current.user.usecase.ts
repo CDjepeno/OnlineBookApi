c0b2cbfab5e7c19410d32751e4ad924be1257867
CDjepeno <djepenodev@gmail.com>
2025-03-13 22:47:30 +0100

    fix: :hammer: ON-183 refactore getCurrentUser.useCase for error

--- apps/nest/src/application/usecases/user/auth/GetCurrentUser/get.current.user.usecase.ts
+++ apps/nest/src/application/usecases/user/auth/GetCurrentUser/get.current.user.usecase.ts
@@ -1,4 +1,8 @@
-import { HttpException } from '@nestjs/common';
+import {
+  InternalServerException,
+  NotFoundException,
+} from 'src/domaine/errors/onlineBook.error';
+import { ErrorsMessagesEnum } from 'src/enums/errors.enums';
 import { UsersRepository } from 'src/repositories/user.repository';
 import { CurrentUserResponse } from './current.user.response';
 
@@ -17,9 +21,18 @@ export class GetCurrentUserUseCase {
       };
       return responses;
     } catch (error) {
-      if (error instanceof HttpException) {
-        // Si c'est une exception NestJS connue, on la relance directement
-        throw error;
+      if (error instanceof Error) {
+        if (error.message === ErrorsMessagesEnum.DATABASE_ERROR) {
+          throw new InternalServerException('Database Error');
+        }
+        if (error.message === ErrorsMessagesEnum.NOT_FOUND) {
+          throw new NotFoundException('Aucun utilisateur courant trouvé');
+        }
+        if (error.message === ErrorsMessagesEnum.INTERNAL_SERVER_ERROR) {
+          throw new InternalServerException(
+            "Probleme serveur impossible recuperer l'utilisateur actuelle",
+          );
+        }
       }
       throw error;
     }
