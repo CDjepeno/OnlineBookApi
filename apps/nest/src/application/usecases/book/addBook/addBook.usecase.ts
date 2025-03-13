76f6c650d5df9f9c982dac41c6b3409cded37b5d
CDjepeno <djepenodev@gmail.com>
2025-03-13 20:12:29 +0100

    fix: :hammer: ON-183 refactore addbook useCase for error

--- apps/nest/src/application/usecases/book/addBook/addBook.usecase.ts
+++ apps/nest/src/application/usecases/book/addBook/addBook.usecase.ts
@@ -1,5 +1,7 @@
 import { HttpException } from '@nestjs/common';
 import { BookEntity } from 'src/domaine/entities/Book.entity';
+import { InternalServerException, NotFoundException } from 'src/domaine/errors/onlineBook.error';
+import { ErrorsMessagesEnum } from 'src/enums/errors.enums';
 import { AwsS3Client } from 'src/infras/clients/aws/aws-s3.client';
 import { BookRepository } from 'src/repositories/book.repository';
 import { AddBookRequest } from './addBook.request';
@@ -28,11 +30,21 @@ export class AddBookUseCase {
 
       return { msg: 'Votre livre a bien été créé' };
     } catch (error) {
-      if (error instanceof HttpException) {
-        // Si c'est une exception NestJS connue, on la relance directement
+      if (error instanceof Error) {
+        if (error.message === ErrorsMessagesEnum.DATABASE_ERROR) {
+          throw new InternalServerException('Database Error');
+        }
+        if (error.message === ErrorsMessagesEnum.NOT_FOUND) {
+          throw new NotFoundException('Utilisateur non trouvé');
+        }
         throw error;
       }
-      throw error;
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerException(
+        "Probleme serveur impossible d'ajouter l'utilisateur",
+      );
     }
   }
 }
