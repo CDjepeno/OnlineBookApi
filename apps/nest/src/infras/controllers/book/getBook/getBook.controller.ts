38ad8ad8dbbac1026b5fbb6b7096d94f24377d1f
robleh <hassan.alirobleh@gmail.com>
2025-04-13 20:19:59 +0200

    feat: :sparkles: ON-195 remove throw in controller

--- apps/nest/src/infras/controllers/book/getBook/getBook.controller.ts
+++ apps/nest/src/infras/controllers/book/getBook/getBook.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  Get,
-  Inject,
-  NotFoundException,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, Get, Inject, Param, ParseIntPipe } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetBookUsecase } from 'src/application/usecases/book/getBook/getBook.usecase';
 import { UseCaseProxy } from 'src/infras/usecase-proxy/usecase-proxy';
@@ -25,10 +18,6 @@ export class GetBookController {
     summary: 'Get Book by Id',
   })
   async getBook(@Param('id', ParseIntPipe) id: number): Promise<GetBookDto> {
-    const book = await this.getBookUsecaseProxy.getInstance().execute(id);
-    if (!book) {
-      throw new NotFoundException(`Aucun livre trouvé avec le nom "${id}"`);
-    }
-    return book;
+    return await this.getBookUsecaseProxy.getInstance().execute(id);
   }
 }
