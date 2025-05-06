ba5e484b6e96d4106aa8ce370a532207b108759b
robleh <hassan.alirobleh@gmail.com>
2025-05-06 19:33:00 +0200

    feat: :hammer: ON-219 Fix Extrait email

--- apps/nest/src/infras/controllers/user/auth.controller.ts
+++ apps/nest/src/infras/controllers/user/auth.controller.ts
@@ -4,13 +4,13 @@ import {
   Get,
   Inject,
   Post,
+  Req,
   UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 
 import { GetCurrentUserUseCase } from 'src/application/usecases/user/auth/get.current.user.usecase';
 import { LoginUserUseCase } from 'src/application/usecases/user/getuser/login.user.usecase';
-import { UserEmail } from 'src/infras/common/decorators/user-email.decorator';
 import { JwtAuthGuard } from 'src/infras/common/guards/jwt-auth.guard';
 import { UseCaseProxy } from 'src/infras/usecase-proxy/usecase-proxy';
 import { UsecaseProxyModule } from 'src/infras/usecase-proxy/usecase-proxy.module';
@@ -38,7 +38,12 @@ export class AuthController {
 
   @Get('current')
   @UseGuards(JwtAuthGuard)
-  async getCurrentUser(@UserEmail() email: string) {
+  async getCurrentUser(@Req() request) {
+    const email = request.user.email;
+    if (!email) {
+      throw new UnauthorizedException('Token invalide ou manquant!');
+    }
+
     return await this.getCurrentUserUseCase.getInstance().execute(email);
   }
 }
